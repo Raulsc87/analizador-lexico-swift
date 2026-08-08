0c825b30a882b85083bc793d2e47f6a0e2c1d854
DiegoUmeso <162086142+DiegoUmeso@users.noreply.github.com>
2026-08-07 23:07:55 -0600

    analizador lexico 2.0
    Se agregan nuevas reglas para reconocer tipos de datos palabras reservadas, operadores, paréntesis, llaves y otros símbolos utilizados en el lenguaje Swift tambien se agrgo la tabla de símbolos y para registra  la información el problema es que le cuesta escanear cuando son 175 líneas de código swift .

--- ejemplo.swift
+++ ejemplo.swift
@@ -1,9 +1,22 @@
+var edad = 2.0
 var edad = 20
+edad = 30
+edad = 40
 let nombre = "Diego"
+var activo = true
+var eliminado = false
 
 if edad >= 18 {
 
 }
 else{
+
+}
+
+var edad = 20
+
+edad += 5
+
+if edad >= 18 && edad != 30 {
 
 }
